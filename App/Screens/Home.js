3759dc7c0511f21d5ebc69535c305d48d411a42a
Siegfred Gamboa <siegy.gamboa@gmail.com>
2024-02-26 05:19:53 +0200

    refactor home page, created a basic layout for home

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -1,24 +1,127 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Pressable,
+} from 'react-native';
+import React, { useState } from 'react';
+import {
+  MaterialCommunityIcons,
+  Foundation,
+  FontAwesome6,
+  Entypo,
+  FontAwesome,
+} from '@expo/vector-icons';
 
-export default function Home() {
+export default function Home(props) {
+  const [number, onChangeNumber] = useState('');
+  const { onPress, title = 'Continue' } = props;
+
   return (
     <View style={styles.container}>
       <View style={styles.sendChoicesContainer}>
-        <Text style={styles.text}>Home</Text>
+        <View style={styles.choicesContainer}>
+          <View style={styles.choicesItem}>
+            <Text>
+              <Foundation name="euro" size={30} color="white" />
+            </Text>
+          </View>
+          <Text style={styles.choicesSubtext}>Send to FI</Text>
+        </View>
+        <View style={styles.choicesContainer}>
+          <View style={styles.choicesItem}>
+            <Text>
+              <FontAwesome6 name="peso-sign" size={20} color="white" />
+            </Text>
+          </View>
+          <Text style={styles.choicesSubtext}>Send to PH</Text>
+        </View>
+        <View style={styles.choicesContainer}>
+          <View style={styles.choicesItem}>
+            <Text>
+              <FontAwesome name="user" size={24} color={'white'} />
+            </Text>
+          </View>
+          <Text style={styles.choicesSubtext}>Send to Self</Text>
+        </View>
+        <View style={styles.choicesContainer}>
+          <View style={styles.choicesItem}>
+            <Text>
+              <Entypo name="mobile" size={24} color="white" />
+            </Text>
+          </View>
+          <Text style={styles.choicesSubtext}>Buy Load</Text>
+        </View>
       </View>
       <View style={styles.ratesContainer}>
         <View style={styles.currencyContainer}>
           <View style={styles.currency}>
-            <Text>1</Text>
+            <Text>FI</Text>
+          </View>
+          <View style={styles.swap}>
+            <MaterialCommunityIcons
+              name="swap-horizontal-circle"
+              size={24}
+              color="black"
+            />
           </View>
           <View style={styles.currency}>
-            <Text>1</Text>
-          </View>
-          <View style={styles.currency}>
-            <Text>1</Text>
+            <Text>PH</Text>
           </View>
         </View>
+        <SafeAreaView>
+          <Text style={styles.inputLabel}>You send</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="Type amount in EUR"
+            keyboardType="numeric"
+            render={() => (
+              <View style={styles.inputContainer}>
+                <MaterialCommunityIcons
+                  name="currency-usd"
+                  size={24}
+                  color="green"
+                />
+                <TextInput
+                  style={styles.inputField}
+                  onChangeText={onChangeNumber}
+                  value={number}
+                  keyboardType="numeric"
+                />
+              </View>
+            )}
+          />
+          <Text style={styles.inputLabel}>They receive</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="Type amount in PHP"
+            keyboardType="numeric"
+            render={() => (
+              <View style={styles.inputContainer}>
+                <MaterialCommunityIcons
+                  name="currency-eur"
+                  size={24}
+                  color="blue"
+                />
+                <TextInput
+                  style={styles.inputField}
+                  onChangeText={onChangeNumber}
+                  value={number}
+                  keyboardType="numeric"
+                />
+              </View>
+            )}
+          />
+        </SafeAreaView>
+        <Pressable style={styles.button} onPress={onPress}>
+          <Text style={styles.buttonText}>{title}</Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -29,19 +132,45 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#6834d4',
   },
-  text: {
-    color: '#010101',
+  choicesContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    gap: 5,
+  },
+  choicesItem: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#35166e',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  choicesSubtext: {
+    width: '100%',
+    fontSize: 10,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 2,
   },
   sendChoicesContainer: {
     backgroundColor: 'white',
     height: 100,
     padding: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
   ratesContainer: {
     backgroundColor: 'white',
-    height: 200,
-    padding: '10px 20px',
-    margin: 20,
+    // height: 200,
+    padding: 20,
+    margin: 10,
     borderRadius: 10,
   },
   currency: {
@@ -54,6 +183,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 4,
   },
   currencyContainer: {
     display: 'flex',
@@ -61,5 +191,39 @@ const styles = StyleSheet.create({
     gap: 10,
     width: '100%',
     justifyContent: 'center',
+  },
+  swap: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    borderRadius: 4,
+    padding: 10,
+    borderWidth: 0.5,
+  },
+  inputLabel: {
+    padding: 5,
+    fontSize: 12,
+    marginTop: 10,
+  },
+
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#35166e',
+    marginTop: 10,
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
